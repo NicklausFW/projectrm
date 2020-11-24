2d0f2365da6f43291ec15d630bb5ffb09a91334d
NicklausFW <nicklaus.fumihiro@yahoo.com>
2020-11-24 14:07:08 +0700

    Push 2

--- blog/views.py
+++ blog/views.py
@@ -3,20 +3,6 @@ from .models import Post
 #from django.http import HttpResponse
 # Create your views here.
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Posts 1',
-        'content': 'First post content',
-        'date_posted': 'August 27,2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Posts 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28,2018'
-    }
-]
 
 # handle traffic from the home page
 def home(request):
