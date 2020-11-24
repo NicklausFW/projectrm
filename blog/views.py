c60c6c883431c7ea1b6e6446eec1f41c5e7cf49b
NicklausFW <nicklaus.fumihiro@yahoo.com>
2020-11-24 14:03:11 +0700

    Push

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post
+#from django.http import HttpResponse
 # Create your views here.
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Posts 1',
+        'content': 'First post content',
+        'date_posted': 'August 27,2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Posts 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28,2018'
+    }
+]
 
 # handle traffic from the home page
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        # 'posts': posts
+        'posts': Post.objects.all
+    }
+    return render(request, 'blog/home.html', context)
+   # return HttpResponse('<h1>Blog Home</h1>')
+
 
 def about(request):
-    return HttpResponse('<h1>About</h1>')
+    return render(request, 'blog/about.html', {'title' : 'About'})
